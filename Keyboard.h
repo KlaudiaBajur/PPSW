b926eac4443db4b09192b47c672ac0f118d0d5d4
KlaudiaBajur <79943592+KlaudiaBajur@users.noreply.github.com>
2021-03-22 19:06:22 +0100

    Update Keyboard.h

--- Keyboard.h
+++ Keyboard.h
@@ -1,4 +1,4 @@
-enum ButtonState {BUTTON_0, BUTTON_1, BUTTON_2, BUTTON_3, RELASED};
+enum ButtonState {RELASED, BUTTON_0, BUTTON_1, BUTTON_2, BUTTON_3};
 
 void KeyboardInit(void);
 enum ButtonState eKeyboardRead(void);
